250758388e30bb10cb00d214de416bd3dc0a1f85
Matthew <mhamilton942@gmail.com>
2019-04-22 20:35:19 +0100

    code refactor
    error: pres[i] = threshold
    add: gitignore

--- stepper_pump_i2s_random_8000hz/stepper_pump_i2s_random_8000hz.ino
+++ stepper_pump_i2s_random_8000hz/stepper_pump_i2s_random_8000hz.ino
@@ -42,7 +42,7 @@ void setup()
   while (!Serial) {}
 
   // start I2S at 16 kHz with 32-bits per sample
-  if (!I2S.begin(I2S_PHILIPS_MODE, 8000, 32)) 
+  if (!I2S.begin(I2S_PHILIPS_MODE, 8000, 32))
   {
     Serial.println("Failed to initialize I2S!");
     while (1); // do nothing
@@ -61,149 +61,116 @@ void setup()
 
 void loop()
 {
-  // read a bunch of samples:
-  int samples[SAMPLES];
-
-  for (int i = 0; i < SAMPLES; i++) {
-    int sample = 0;
-    while ((sample == 0) || (sample == -1) ) {
-      sample = I2S.read();
-      //      Serial.println(sample);
-    }
-    sample >>= 14;
-    samples[i] = sample;
-  }
-
-  // ok we hvae the samples, get the mean (avg)
-  float meanval = 0;
-  for (int i = 0; i < SAMPLES; i++) {
-    meanval += samples[i];
-  }
-  meanval /= SAMPLES;
-  //Serial.print("# average: " ); Serial.println(meanval);
-
-  // subtract it from all sapmles to get a 'normalized' output
-  for (int i = 0; i < SAMPLES; i++) {
-    samples[i] -= meanval;
-  }
-
-  // find the 'peak to peak' max
-  float maxsample, minsample;
-  minsample = 100000;
-  maxsample = -100000;
-  for (int i = 0; i < SAMPLES; i++) {
-    minsample = min(minsample, samples[i]);
-    maxsample = max(maxsample, samples[i]);
-  }
-  rawVal = maxsample - minsample;
-
+  rawVal = getLoudness();
 
   pres[count] = rawVal;
   count += 1;
 
-
-  if (count >= (presNum - 1) ) {
-    for (int i = 0; i < (presNum - 1); i++) {
-      if (pres[i] >= threshold) {
+  if (count >= (presNum - 1) )
+  {
+    for (int i = 0; i < (presNum - 1); i++)
+    {
+      if (pres[i] >= threshold)
+      {
         pre += 1;
-        if (pres[i] = threshold) {
+        if (pres[i] == threshold) //was previously pres[i] = threshold
+        {
           p200 += 1;
         }
       }
       pres[i] = pres[i + 1];
     }
 
-    if ((pres[presNum - 4] > threshold) && (rawVal < threshold)) {
+    if ((pres[presNum - 4] > threshold) && (rawVal < threshold))
+    {
       t0 = millis();
     }
 
-    // t0>t1
-    if (rawVal < threshold) {
-      if ((millis() - t0) < 5000) {
-        if (pre <= 20)  {
-          avgVal = rawVal;
-        }
-        else {
-          avgVal = 500;
-        }
+    if (rawVal < threshold) // t0>t1
+    {
+      if ((millis() - t0) < 5000)
+      {
+        avgVal = (pre <= 20) ? rawVal : 500      
       }
-      else {
+      else
+      {
         avgVal = rawVal;
       }
     }
-
-    // rawVal > threshold, t1>t0
-    else {
-      //      avgVal = rawVal;
-      if (pre <= 20) {
-        avgVal = 0;
-      }
-      else {
-        avgVal = rawVal;
-      }
+    else // rawVal > threshold, t1>t0
+    {
+      avgVal = (pre <= 20) ? 0 : rawVal      
     }
 
     count = presNum - 1;
     pre = 0;
     p200 = 0;
   }
-
-
-
+  //---------------------------------------------------------------
   Serial.print(rawVal);
   Serial.print(" ");
   Serial.print(avgVal);
   Serial.print(" ");
   Serial.println(threshold);
+  //---------------------------------------------------------------
 
-
-  if (avgVal >= threshold) {
+  if (avgVal >= threshold)
+  {
     digitalWrite(ledPin1, LOW);
-    if (runFlag == 1) {
+    if (runFlag == 1)
+    {
       myPump->run(RELEASE);
       runFlag = 0;
     }
     myStepper->release();
-
   }
-  else {
+  else
+  {
     digitalWrite(ledPin1, HIGH);
-    if (runFlag == 0) {
+    if (runFlag == 0)
+    {
       myPump->setSpeed(200);
       myPump->run(FORWARD);
       myStepper->setSpeed(10);
       runFlag = 1;
     }
-    if (pos == steps) {
+    if (pos == steps)
+    {
       myStepper->release();
       pos = 0;
       steps = random (1, (stepperRange - 1));
-      if (des == 0) {
+      if (des == 0)
+      {
         dir = 1;
       }
-      else if (des == stepperRange) {
+      else if (des == stepperRange)
+      {
         dir = 2;
       }
-      else {
+      else
+      {
         dir = (dir == 1 ? 2 : 1);
       }
-      if (dir == 1) {
+      if (dir == 1)
+      {
         des = des + steps;
       }
-      if (dir == 2) {
+      if (dir == 2)
+      {
         des = des - steps;
       }
-      if (des < 0) {
+      if (des < 0)
+      {
         steps = steps + des;
         des = 0;
       }
-      if (des > stepperRange) {
+      if (des > stepperRange)
+      {
         steps = stepperRange - (des - steps);
         des = stepperRange;
       }
     }
     myStepper->onestep(dir, MICROSTEP);
-    pos++;
-    //    Serial.println(avgVal);
+    pos++;    
   }
 }
